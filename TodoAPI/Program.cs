2bf8ab6b2751c42db6eaebc1dbeaed76f2bb9c82
lakshmadu <lakshmadu2000@gmail.com>
2023-11-21 15:39:53 +0530

    aded cors policy from configuration

--- TodoAPI/Program.cs
+++ TodoAPI/Program.cs
@@ -1,11 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using TodoAPI.Common;
 using TodoAPI.DataAccess;
 using TodoAPI.Mappers;
 using TodoAPI.Services.TaskItems;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 // Add services to the container.
 
@@ -25,12 +25,14 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 //var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
+var allowedOrigins = GetAllowedOrigins();
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.WithOrigins(allowedOrigins)
             .AllowAnyMethod()
             .AllowAnyHeader();
         });
@@ -59,3 +61,17 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+string[] GetAllowedOrigins()
+{
+    var origins = builder.Configuration.GetSection("AllowedOrigins").Value;
+
+    if (!string.IsNullOrEmpty(origins))
+    {
+        var splittedOrigins = origins.Split(",");
+        return splittedOrigins;
+    }
+
+    return null;
+}
